51c6aad37748927edbdef1ae2391de7a1ba1fceb
Collins Igeh <igehac@gmail.com>
2022-11-19 13:00:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // App vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Collins Igeh";
+            string appPlayer = "";
+            int correctNumber = 7;
+            int guessedNumber = 0;
+
+            // App intro
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+            appPlayer = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", appPlayer);
+
+            // Begin game
+            Console.Write("\nGuess a number between 1 and 10: ");
+
+            // Game checks
+            while (guessedNumber != correctNumber)
+            {
+                guessedNumber = Int32.Parse(Console.ReadLine());
+
+                if(guessedNumber != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number. Try again!");
+                    Console.ResetColor();
+                }
+            }
+
+            // End game
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("You are CORRECT!");
+            Console.ResetColor();
+        }
+    }
+}
