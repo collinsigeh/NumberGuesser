eebe08730a159421feb454589d042f39bbb6618b
Collins Igeh <igehac@gmail.com>
2022-11-19 16:13:13 +0100

    Cleaned up code with methods

--- Program.cs
+++ Program.cs
@@ -4,35 +4,19 @@
     {
         static void Main(string[] args)
         {
-            // App vars
-            string appName = "Number Guesser";
-            string appVersion = "1.0.0";
-            string appAuthor = "Collins Igeh";
-            string appPlayer = "";
-            string replay = "";
-            int correctNumber = 0;
-            int guessedNumber = 0;
+            DisplayAppInfo();
+            GreetPlayer();
 
-            // App intro
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
-            Console.ResetColor();
-
-            while (string.IsNullOrEmpty(appPlayer))
-            {
-                Console.WriteLine("What is your name?");
-                appPlayer = Console.ReadLine();
-            }
-
-            Console.WriteLine("Hello {0}, let's play a game...", appPlayer);
-
-            while(true)
+            while (true)
             {
                 // Begin game
-                replay = "N";
+                string replay = "N";
+
                 Random random = new Random();
-                correctNumber = random.Next(1, 11);
-                Console.Write("\nGuess a number between 1 and 10: ");
+                int correctNumber = random.Next(1, 11);
+                int guessedNumber = 0;
+
+                Console.WriteLine("\nGuess a number between 1 and 10:");
 
                 // Game checks
                 while (guessedNumber != correctNumber)
@@ -40,9 +24,7 @@
                     //guessedNumber = Int32.Parse(Console.ReadLine());
                     if (!int.TryParse(Console.ReadLine(), out guessedNumber) || guessedNumber < 1 || guessedNumber > 10)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Invalid entry. Try again!");
-                        Console.ResetColor();
+                        PrintColorMessage(ConsoleColor.Red, "Invalid entry. Try again!");
 
                         continue;
                     }
@@ -56,11 +38,9 @@
                 }
 
                 // End game
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("You are CORRECT!");
-                Console.ResetColor();
+                PrintColorMessage(ConsoleColor.Yellow, "Your are CORRECT!");
 
-                Console.Write("\nPress \"Y\" to play again.");
+                Console.Write("\nPlay Again? [Y or N]: ");
                 replay = Console.ReadLine().ToUpper();
 
                 if (replay == "Y")
@@ -72,6 +52,39 @@
                     return;
                 }
             }
+        }
+
+        // Get and displa app info
+        static void DisplayAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Collins Igeh";
+
+            string greeting = appName + ": Version" + appVersion + " by " + appAuthor;
+            PrintColorMessage(ConsoleColor.Green, greeting);
+        }
+
+        // Request player's name and greet player
+        static void GreetPlayer()
+        {
+            string appPlayer = "";
+
+            while (string.IsNullOrEmpty(appPlayer))
+            {
+                Console.WriteLine("What is your name?");
+                appPlayer = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello {0}, let's play a game...", appPlayer);
+        }
+
+        // Prints a message to the console with specified colour
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
